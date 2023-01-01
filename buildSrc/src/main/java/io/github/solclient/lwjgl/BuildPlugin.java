bf1d105853672a455ee1363c985d87112b3b9d9f
TheKodeToad <TheKodeToad@proton.me>
2023-01-01 10:23:55 +0000

    Add NanoVG

--- buildSrc/src/main/java/io/github/solclient/lwjgl/BuildPlugin.java
+++ buildSrc/src/main/java/io/github/solclient/lwjgl/BuildPlugin.java
@@ -1,13 +1,13 @@
 package io.github.solclient.lwjgl;
 
-import org.gradle.api.Plugin;
-import org.gradle.api.Project;
+import org.gradle.api.*;
 
 public class BuildPlugin implements Plugin<Project> {
 
 	@Override
 	public void apply(Project project) {
-		project.getTasks().register("build").get().dependsOn(project.getTasks().register("assemble").get().dependsOn(project.getTasks().register("patch", PatchTask.class)));
+		project.getTasks().register("build").get().dependsOn(project.getTasks().register("assemble").get()
+				.dependsOn(project.getTasks().register("patch", PatchTask.class)));
 	}
 
 }
--- buildSrc/src/main/java/io/github/solclient/lwjgl/PatchTask.java
+++ buildSrc/src/main/java/io/github/solclient/lwjgl/PatchTask.java
@@ -1,50 +1,43 @@
 package io.github.solclient.lwjgl;
 
+import java.io.*;
+import java.util.*;
+import java.util.zip.*;
+
 import org.apache.commons.io.IOUtils;
 import org.gradle.api.DefaultTask;
 import org.gradle.api.tasks.TaskAction;
-import org.objectweb.asm.ClassReader;
-import org.objectweb.asm.ClassWriter;
-import org.objectweb.asm.commons.ClassRemapper;
-import org.objectweb.asm.commons.Remapper;
-
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipFile;
-import java.util.zip.ZipOutputStream;
+import org.objectweb.asm.*;
+import org.objectweb.asm.commons.*;
 
 public class PatchTask extends DefaultTask {
 
 	private final File outputFile;
 	private final File coreFile;
 	private final File stbFile;
+	private final File nanoVgFile;
 
 	public PatchTask() {
 		File libs = new File(getProject().getBuildDir(), "libs");
 		outputFile = new File(libs, "lwjgl-patched-all.jar");
 		coreFile = new File(libs, "lwjgl-core.jar");
 		stbFile = new File(libs, "lwjgl-stb.jar");
+		nanoVgFile = new File(libs, "lwjgl-nanovg.jar");
 	}
 
 	private static void combine(File outputFile, File... files) throws IOException {
 		ZipFile[] zips = new ZipFile[files.length];
 
-		for(int index = 0; index < files.length; index++) {
+		for (int index = 0; index < files.length; index++) {
 			zips[index] = new ZipFile(files[index]);
 		}
 
 		Set<String> seenNames = new HashSet<>();
 
-		try(ZipOutputStream out = new ZipOutputStream(new FileOutputStream(outputFile))) {
-			for(ZipFile zip : zips) {
-				for(ZipEntry entry : Collections.list(zip.entries())) {
-					if(entry.isDirectory() || !seenNames.add(entry.getName())) {
+		try (ZipOutputStream out = new ZipOutputStream(new FileOutputStream(outputFile))) {
+			for (ZipFile zip : zips) {
+				for (ZipEntry entry : Collections.list(zip.entries())) {
+					if (entry.isDirectory() || !seenNames.add(entry.getName())) {
 						continue;
 					}
 
@@ -59,18 +52,19 @@ public class PatchTask extends DefaultTask {
 	}
 
 	private static void process(File inFile, File outFile) throws IOException {
-		try(ZipFile in = new ZipFile(inFile); ZipOutputStream out = new ZipOutputStream(new FileOutputStream(outFile))) {
-			for(ZipEntry entry : Collections.list(in.entries())) {
-				if(entry.isDirectory()) {
+		try (ZipFile in = new ZipFile(inFile);
+				ZipOutputStream out = new ZipOutputStream(new FileOutputStream(outFile))) {
+			for (ZipEntry entry : Collections.list(in.entries())) {
+				if (entry.isDirectory()) {
 					continue;
 				}
 				// just like with the old mods
-				if(entry.getName().startsWith("META-INF/")) {
+				if (entry.getName().startsWith("META-INF/")) {
 					continue;
 				}
 
 				InputStream entryIn = in.getInputStream(entry);
-				if(entry.getName().endsWith(".class")) {
+				if (entry.getName().endsWith(".class")) {
 					byte[] data = IOUtils.toByteArray(entryIn);
 					ClassReader reader = new ClassReader(data);
 					ClassWriter writer = new ClassWriter(0);
@@ -84,10 +78,10 @@ public class PatchTask extends DefaultTask {
 
 					}), 0);
 					data = writer.toByteArray();
-					out.putNextEntry(new ZipEntry(replace(entry.getName().substring(0, entry.getName().lastIndexOf('.'))) + ".class"));
+					out.putNextEntry(new ZipEntry(
+							replace(entry.getName().substring(0, entry.getName().lastIndexOf('.'))) + ".class"));
 					out.write(data);
-				}
-				else {
+				} else {
 					out.putNextEntry(new ZipEntry(entry.getName()));
 					IOUtils.copy(entryIn, out);
 				}
@@ -97,15 +91,15 @@ public class PatchTask extends DefaultTask {
 	}
 
 	private static String replace(String string) {
-		if(string.endsWith("org/lwjgl/BufferUtils")
-				|| string.endsWith("org/lwjgl/PointerBuffer")) {
+		if (string.endsWith("org/lwjgl/BufferUtils") || string.endsWith("org/lwjgl/PointerBuffer")) {
 			return string.replace("lwjgl", "lwjgl3");
 		}
 		return string;
 	}
 
 	private static File getDependency(Set<File> deps, String prefix) { // quick-n-dirty
-		return deps.stream().filter((dep) -> dep.getName().substring(0, dep.getName().lastIndexOf('-')).equals(prefix)).findFirst().get();
+		return deps.stream().filter((dep) -> dep.getName().substring(0, dep.getName().lastIndexOf('-')).equals(prefix))
+				.findFirst().get();
 	}
 
 	@TaskAction
@@ -114,7 +108,8 @@ public class PatchTask extends DefaultTask {
 		Set<File> deps = getProject().getConfigurations().getByName("lwjgl").resolve();
 		process(getDependency(deps, "lwjgl"), coreFile);
 		process(getDependency(deps, "lwjgl-stb"), stbFile);
-		combine(outputFile, coreFile, stbFile);
+		process(getDependency(deps, "lwjgl-nanovg"), nanoVgFile);
+		combine(outputFile, coreFile, stbFile, nanoVgFile);
 	}
 
 }
